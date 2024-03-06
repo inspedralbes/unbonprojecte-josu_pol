2f57716e4325e0e42f3f6741b5cb41430e5b4ab0
Polete23 <a20poldonpoz@inspedralbes.cat>
2024-03-06 09:15:45 +0100

    espacio

--- laravel/laravel_cars/app/Http/Controllers/CocheController.php
+++ laravel/laravel_cars/app/Http/Controllers/CocheController.php
@@ -7,18 +7,14 @@ use Illuminate\Http\Request;
 
 class CocheController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $coches = Coche::all();
         return response()->json(['coches' => $coches], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $request->validate([
@@ -36,9 +32,7 @@ class CocheController extends Controller
         return response()->json(['coche' => $coche], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         $coche = Coche::find($id);
@@ -50,9 +44,7 @@ class CocheController extends Controller
         return response()->json(['coche' => $coche], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         $request->validate([
@@ -76,9 +68,7 @@ class CocheController extends Controller
         return response()->json(['coche' => $coche], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(string $id)
     {
         $coche = Coche::find($id);
